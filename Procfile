fb4f4e27578fd5c74b18a627fd7768354d168180
Gagandeep1051 <Gagandeep1051@gmail.com>
2022-06-17 16:42:57 -0400

    Add existing file

---
+++ Procfile
@@ -0,0 +1,3 @@
+web:node src/server.js 
+web:npm start && npm start
+
--- src/server.js
+++ src/server.js
@@ -90,4 +90,5 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname + '/build/index.html'));
 })
 
-app.listen(8000, () => console.log("listening on 8000"));
+var port = process.env.PORT || 8000;
+app.listen(port, "0.0.0.0", () => console.log("listening on 8000"));
