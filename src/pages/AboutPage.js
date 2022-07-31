66d520c44fab928c897160e138288914c31349c6
Gagandeep1051 <Gagandeep1051@gmail.com>
2022-07-31 03:06:46 -0400

    about message

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -15,28 +15,39 @@ const AboutPage = () =>(
       <h1 id="about1">About me</h1>
 
          <p id="P1">Hello,
-                My name is Gagandeep Singh. I wanted to introduce myself and let you know a
-                little bit about myself. I am a current graduate from Queens College with my 
-                Bachelor's degree. I am an effective student committed to learning, developing 
-                skills in web development and team contribution. I am self-directed and energetic 
-                with superior performance in both autonomous or collaborative environments working
-                independently and collaborating with others on group projects. I am a good fit for 
-                this position because I love to learn and help others learn. I am currently working 
-                on learning to develop in React and TypeScript projects.
+                    I am Gagandeep Singh. I needed to present myself and let you in on a tad about myself. 
+                    I'm an ongoing alumni from Queens College with my Bachelor's certification. I'm a viable 
+                    understudy focused on getting the hang of, creating abilities in web improvement and group 
+                    commitment. I'm independent and enthusiastic with predominant execution in both independent 
+                    or cooperative conditions working freely and teaming up with others on bunch projects. I'm 
+                    ideal for this position since I love to learn and help other people learn. Right now, 
+                    I'm working with figuring out how to create React and TypeScript projects.
+
             </p>
 
-            <p id="P2"> On my last project, I created an application which supported distance-remote learning 
-                for students. I used Html, Css, Node.js and Express, and mySql and mongoose db. Users 
-                can log in and log out, I integrated user auth using passport to create persistent logins.
+            <p id="P2"> On my last undertaking, I made an application which upheld distance-remote learning for 
+            understudies. I utilized Html, Css, Node.js and Express, and mySql and mongoose db. Clients can sign
+             in and log out, I coordinated client auth utilizing visa to make tireless logins. The two clients, 
+             understudy and educators have various entrances. Understudies can sign up for class by mentioning 
+             the instructors, and are possibly selected assuming the educator acknowledges their solicitations.
+             Understudies can then see every one of the tasks allocated by the instructors. They can present their
+              works to those educators and furthermore view every one of the grades. Understudies can likewise see
+               announcements made by the instructors too. Educators can select understudies and eliminate them if 
+               necessary. They can make new classes and new timetables for the understudies. they can likewise 
+               relegate tasks to the entire class and can then grade every one of the tasks. they can likewise 
+               leave announcements for the understudies.
+
+
             </p>
 
             <p id="P3">
                 
-                Last month, I joined a DevPath Hackathon where my team and I were placed first position.
-                We created a web app in react and android application in java called Parkout. Park Out is 
-                a user-friendly website/application which allows any user the ability to find an available 
-                parking spot near their location using the enlisted parking spots. This is done so by the 
-                opposing user who has reserved a parking spot for the arriving user.
+            I joined a DevPath Hackathon where my group and I were put first position. We made a web application 
+            in response and an android application in java called Parkout. Park Out is an easy to understand 
+            site/application which permits any client the capacity to find an accessible parking space close 
+            to their area utilizing the enrolled parking spaces. This is done as such by the restricting client 
+            who has saved a parking space for the showing up client.
+
             </p>
 
                 <h4 id="last-header">And much more coming soon...</h4>
